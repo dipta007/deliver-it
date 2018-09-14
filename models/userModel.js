a2f763caeb45010ae2c13e54a44180c18f8ac0f7
dipta007 <iamdipta@gmail.com>
2018-09-14 15:01:48 +0600

    initial commit

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+    fullName:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    hash_password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    phoneNo: {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.hash_password);
+}
+
+mongoose.model('User', UserSchema);
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = function(app) {
+    var userHandler = require('../controller/userController');
+
+    app.route('/auth/register')
+        .post(userHandler.register)
+
+    app.route('/auth/sign_in')
+        .post(userHandler.signIn)
+
+};
