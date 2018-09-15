ce80c607ae4a73a95bf95fd544336a96e7095d6c
dipta007 <iamdipta@gmail.com>
2018-09-15 07:50:56 +0600

    options prefleight flag handled

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ var routes = require('./routes/routes');
 routes(app);
 
 app.use(function(req, res) {
+    if (req.method === 'OPTIONS') {
+        console.log('!OPTIONS');
+        var headers = {};
+        // IE8 does not allow domains to be specified, just the *
+        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        res.writeHead(200, headers);
+        res.end();
+  }
   res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
 
     app.route('/auth/register')
         .post(userHandler.register)
+        
 
     app.route('/auth/sign_in')
         .post(userHandler.signIn)
